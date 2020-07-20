198ae9f799e673fffec0dbdd232a95bbbdc260a7
yesnoex <32733493+yesnoex@users.noreply.github.com>
2020-07-20 21:30:57 +0800

    加入特效與物理性質 以及認識語法

---
+++ Pgame/New Unity Project/Assets/Script/car.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class car : MonoBehaviour
+{
+    
+    //欄位 field:儲存資料
+    //欄位語法
+    //欄位類型 欄位名稱 節數符號
+    
+    //整數 int:沒有小數點的數值，例: 1、100、-999
+    //浮點數 float:有小數點的數值，例:1.234、-999.9
+    //字串 string: 文字：紅水、kid@gamil.com、3213salkj
+    //布林值 bool：是、否,例:true、false
+
+    //資料
+    //品牌、CC 數、重量、顏色、速度、是否有天窗
+    string brand;
+    int cc;
+    float weight;
+    bool window;
+    float speed;
+
+
+}
