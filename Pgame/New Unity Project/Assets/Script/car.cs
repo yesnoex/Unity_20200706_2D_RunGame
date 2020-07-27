ccf85d26ca7b25881befb6983e347ee0c2b4bcd2
yesnoex <32733493+yesnoex@users.noreply.github.com>
2020-07-27 21:34:44 +0800

    程是屬性

--- Pgame/New Unity Project/Assets/Script/car.cs
+++ Pgame/New Unity Project/Assets/Script/car.cs
@@ -1,25 +1,25 @@
-﻿using UnityEngine;
+﻿// using UnityEngine;
 
-public class car : MonoBehaviour
-{
+// public class car : MonoBehaviour
+// {
     
-    //欄位 field:儲存資料
-    //欄位語法
-    //欄位類型 欄位名稱 節數符號
+//     //欄位 field:儲存資料
+//     //欄位語法
+//     //欄位類型 欄位名稱 節數符號
     
-    //整數 int:沒有小數點的數值，例: 1、100、-999
-    //浮點數 float:有小數點的數值，例:1.234、-999.9
-    //字串 string: 文字：紅水、kid@gamil.com、3213salkj
-    //布林值 bool：是、否,例:true、false
+//     //整數 int:沒有小數點的數值，例: 1、100、-999
+//     //浮點數 float:有小數點的數值，例:1.234、-999.9
+//     //字串 string: 文字：紅水、kid@gamil.com、3213salkj
+//     //布林值 bool：是、否,例:true、false
 
-    //資料
-    //品牌、CC 數、重量、顏色、速度、是否有天窗
-    public string brand = "賓士" ;
-    [Hearder("CC 數"), Tooltip(0, 2000)]
-    private int cc = 1500;
-    float weight;
-    bool window;
-    float speed;
+//     //資料
+//     //品牌、CC 數、重量、顏色、速度、是否有天窗
+//     public string brand = "賓士" ;
+//     [Hearder("CC 數"), Tooltip(0, 2000)]
+//     private int cc = 1500;
+//     float weight;
+//     bool window;
+//     float speed;
 
-    public int Cc { get => cc; set => cc = value; }
-}
+//     public int Cc { get => cc; set => cc = value; }
+// }
---
+++ Pgame/New Unity Project/Assets/Script/player.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{ 
+#region 欄位
+[Header("移動速度"), Range(0,1000)]
+public float speed = 5;
+[Header("跳越高度"), Range(0,1000)]
+public int jump = 350 ;
+[Header("血量"), Range(0,2000)]
+public float hp = 500;
+
+public bool isGround;
+    [Header("血量"), Range(0, 2000)]
+    public int coin;
+
+ [Header("音效區域")]
+    public AudioClip 受傷音效;
+    public AudioClip 滑行音效;
+    public AudioClip 跳躍音效;
+    public AudioClip 金幣音效;
+ [Header("動畫區域")]
+    public Animator 動畫控制器;
+[Header("物理性質")]
+public Rigidbody2D 剛體;
+    public CapsuleCollider2D 碰撞器;
+    #endregion
+
+#region 方法
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void MOVE()
+    {
+
+    }
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+
+    }
+    /// <summary>
+    /// 滑行
+    /// </summary>
+    private void Slide()
+    {
+
+    }
+    /// <summary>
+    /// 吃金幣
+    /// </summary>
+    private void EatCoin()
+    {
+
+    }
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    private void Hit()
+    {
+
+    }
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    private void Dead()
+    {
+
+    }
+    /// <summary>
+    /// 過關
+    /// </summary>
+    private void Pass()
+    {
+
+    }
+
+
+#endregion
+
+#region 事件
+
+
+
+
+
+
+#endregion
+
+
+
+
+}
